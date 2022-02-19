595f226f68ae84ef27ebf7a3bff5246a259fa65e
Eric Greer <eric.greer@comcast.com>
2022-02-18 17:07:22 -0800

    working output

--- cmd/command-piping/main.go
+++ cmd/command-piping/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"os/exec"
 	"strings"
 	"time"
@@ -27,43 +26,38 @@ func run() ([]string, error) {
 	c1 := exec.CommandContext(ctx, "cat", "link-output.txt")
 	c2 := exec.CommandContext(ctx, "grep", "-B", "2", "dummy")
 
-	// map the processes together with a pipe
+	// pipe the first process to the second process
 	var err error
 	c2.Stdin, err = c1.StdoutPipe()
 	if err != nil {
-		return []string{}, fmt.Errorf("ipManager: error creating pipe from process 1 to 2: %w", err)
+		panic(err)
 	}
 
 	// make an output buffer and attach it to the output of command 2
-	b2 := bytes.NewBuffer([]byte{})
-	c2.Stdout = b2
+	outputBuf := bytes.NewBuffer([]byte{})
+	c2.Stdout = outputBuf
 
-	// start the main processes
-	log.Debugln("ipManager: starting process 1 (ip -details link show)")
+	// start the second process that will read from the first process
+	err = c2.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	// start the first that pipes to the second and wait for it to finish
 	err = c1.Run()
 	if err != nil {
-		return []string{}, fmt.Errorf("ipManager: error running ip to retrieve interfaces: %w", err)
+		panic(err)
 	}
 
-	// start the grep command
-	log.Debugln("ipManager: starting process 2 (grep -B 2 dummy)")
-	err = c2.Run()
+	// wait for the second process to finish
+	err = c2.Wait()
 	if err != nil {
-		if !strings.Contains(err.Error(), "exit status 1") {
-			log.Debugln("ipManager: process 2 completed with error:", err)
-			// if golang accepts empty input to a pipe (in our case, no ifaces)
-			// it errs exit 1. Return no ifaces
-			return []string{}, err
-		}
-		log.Warningln("ipManager: found no dummy interfaces with exit message:", err)
-		return []string{}, nil
+		panic(err)
 	}
-
-	log.Debugln("ipManager: got", b2.Len(), "bytes from grep output")
 
 	// list over the interfaces parsed from CLI output and append them into a slice
 	iFaces := []string{}
-	b2SplFromLines := strings.Split(b2.String(), "\n")
+	b2SplFromLines := strings.Split(outputBuf.String(), "\n")
 	for _, l := range b2SplFromLines {
 		if strings.Contains(l, "mtu") {
 			awked := strings.Split(l, " ")
